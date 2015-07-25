58c0b43d929bb318ac6f7baea98f0654c2ca0602
Wenbang Wang <wenbang.wang@gmail.com>
2015-07-25 12:59:21 -0400

    main

---
+++ src/com/wwb/leetcode/utils/main.java
@@ -0,0 +1,12 @@
+package com.wwb.leetcode.utils;
+
+import com.wwb.leetcode.easy.*;
+import com.wwb.leetcode.medium.*;
+import com.wwb.leetcode.hard.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
