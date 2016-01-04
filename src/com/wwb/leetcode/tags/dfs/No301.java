c6129e963ed8797c8459635b5e0cd7546877c450
unknown <212399290@HCU-2UA5192FY1.logon.ds.ge.com>
2016-01-04 09:37:30 -0500

    299 300 301

---
+++ src/com/wwb/leetcode/tags/dfs/No301.java
@@ -0,0 +1,131 @@
+package com.wwb.leetcode.tags.dfs;
+
+import java.util.*;
+
+/**
+ * Remove the minimum number of invalid parentheses in order to make the input string valid.
+ * Return all possible results.
+ *
+ * Note: The input string may contain letters other than the parentheses ( and ).
+ *
+ * Examples:
+ * "()())()" -> ["()()()", "(())()"]
+ * "(a)())()" -> ["(a)()()", "(a())()"]
+ * ")(" -> [""]
+ */
+public class No301 {
+
+    public List<String> removeInvalidParentheses(String s) {
+//        return solution1(s);
+        return solution2(s);
+    }
+
+    private List<String> solution1(String s) {
+        if(s == null) {
+            return Collections.emptyList();
+        }
+
+        List<String> result = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        boolean found = false;
+
+        queue.add(s);
+        visited.add(s);
+
+        while(!queue.isEmpty()) {
+            String str = queue.poll();
+
+            if(isValid(str)) {
+                result.add(str);
+                found = true;
+            }
+
+            if(found) {
+                continue;
+            }
+
+            for(int i = 0, length = str.length(); i < length; i++) {
+                char c = str.charAt(i);
+
+                if(c != '(' && c != ')') {
+                    continue;
+                }
+
+                String currentStr = str.substring(0, i) + str.substring(i + 1);
+
+                if(visited.add(currentStr)) {
+                    queue.add(currentStr);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isValid(String s) {
+        int count = 0;
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                count++;
+            }
+            if(c == ')' && count-- == 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
+
+    private List<String> solution2(String s) {
+        if(s == null) {
+            return Collections.emptyList();
+        }
+
+        int numberOfExtraLeftParen = 0;
+        int numberOfExtraRightParen = 0;
+        Set<String> result = new HashSet<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                numberOfExtraLeftParen++;
+            }
+            if(c == ')') {
+                if(numberOfExtraLeftParen != 0) {
+                    numberOfExtraLeftParen--;
+                } else {
+                    numberOfExtraRightParen++;
+                }
+            }
+        }
+
+        dfs(s, 0, numberOfExtraLeftParen, numberOfExtraRightParen, 0, result, new StringBuilder());
+
+        return new ArrayList<>(result);
+    }
+
+    private void dfs(String s, int pointer, int numberOfExtraLeftParen, int numberOfExtraRightParen, int openParen, Set<String> result, StringBuilder stringBuilder) {
+        if(pointer == s.length() && numberOfExtraLeftParen == 0 && numberOfExtraRightParen == 0 && openParen == 0) {
+            result.add(stringBuilder.toString());
+            return;
+        }
+
+        if(pointer == s.length() || numberOfExtraLeftParen < 0 || numberOfExtraRightParen < 0 || openParen < 0) {
+            return;
+        }
+
+        char c = s.charAt(pointer);
+
+        if(c == '(') {
+            dfs(s, pointer + 1, numberOfExtraLeftParen - 1, numberOfExtraRightParen, openParen, result, stringBuilder);
+            dfs(s, pointer + 1, numberOfExtraLeftParen, numberOfExtraRightParen, openParen + 1, result, stringBuilder.append(c));
+        } else if(c == ')') {
+            dfs(s, pointer + 1, numberOfExtraLeftParen, numberOfExtraRightParen - 1, openParen, result, stringBuilder);
+            dfs(s, pointer + 1, numberOfExtraLeftParen, numberOfExtraRightParen, openParen - 1, result, stringBuilder.append(c));
+        } else {
+            dfs(s, pointer + 1, numberOfExtraLeftParen, numberOfExtraRightParen, openParen, result, stringBuilder.append(c));
+        }
+
+        stringBuilder.setLength(stringBuilder.length() - 1);
+    }
+}
--- src/com/wwb/leetcode/utils/main.java
+++ src/com/wwb/leetcode/utils/main.java
@@ -4,15 +4,14 @@ import com.wwb.leetcode.easy.*;
 import com.wwb.leetcode.medium.*;
 import com.wwb.leetcode.hard.*;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class main {
 
     public static void main(String[] args) {
-        int[] array = {5,9,3,2,1,0,2,3,3,1,0,0};
-        No45 no45 = new No45();
-
-        System.out.println(no45.jump(array));
+        String s = "(";
+        System.out.println(s.substring(0, 0) + s.substring(1));
     }
 }
