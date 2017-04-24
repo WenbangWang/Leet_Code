2db3fa7d6ecd935a141f70d6d15080a991fe8e2f
WenbangWang <wenbang.wang@gmail.com>
2017-04-23 21:35:24 -0700

    update 358

--- src/com/wwb/leetcode/hard/No358.java
+++ src/com/wwb/leetcode/hard/No358.java
@@ -1,9 +1,35 @@
 package com.wwb.leetcode.hard;
 
+/**
+ * Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
+ *
+ * All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string "".
+ *
+ * Example 1:
+ * s = "aabbcc", k = 3
+ *
+ * Result: "abcabc"
+ *
+ * The same letters are at least distance 3 from each other.
+ * Example 2:
+ * s = "aaabc", k = 3
+ *
+ * Answer: ""
+ *
+ * It is not possible to rearrange the string.
+ * Example 3:
+ * s = "aaadbbcc", k = 2
+ *
+ * Answer: "abacabcd"
+ *
+ * Another possible answer is: "abcabcda"
+ *
+ * The same letters are at least distance 2 from each other.
+ */
 public class No358 {
 
     public String rearrangeString(String str, int k) {
-        if (str == null || str.isEmpty() || k <= 0) {
+        if (str == null || str.isEmpty() || k < 0) {
             return "";
         }
 
@@ -23,7 +49,7 @@ public class No358 {
             }
 
             counts[maxPosition]--;
-            positions[maxPosition] = maxPosition + offset;
+            positions[maxPosition] = k + offset;
 
             stringBuilder.append((char) ('a' + maxPosition));
         }
