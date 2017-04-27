68e62db1e08242af89c3cbfc87cf88ec6b9f4cf9
WenbangWang <wenbang.wang@gmail.com>
2017-04-26 23:01:52 -0700

    167,494,523

--- src/com/wwb/leetcode/tags/dp/No494.java
+++ src/com/wwb/leetcode/tags/dp/No494.java
@@ -35,15 +35,56 @@ public class No494 {
     }
 
     private int subsetSum(int[] nums, int target) {
-        int[] dp = new int[target + 1];
-        dp[0] = 1;
-
-        for (int num : nums) {
-            for (int i = target; i >= num; i--) {
-                dp[i] += dp[i - num];
+//        int[] dp = new int[target + 1];
+//        dp[0] = 1;
+//
+//        for (int num : nums) {
+//            for (int i = target; i >= num; i--) {
+//                dp[i] += dp[i - num];
+//            }
+//        }
+//
+//        return dp[target];
+        int[][] dp = new int[nums.length + 1][target + 1];
+        dp[0][0] = 1;
+        for(int i = 1; i <= nums.length; i++) {
+            if(nums[i - 1] == 0) {
+                dp[i][0] = dp[i - 1][0] * 2;
+            } else {
+                dp[i][0] = dp[i - 1][0];
             }
         }
 
-        return dp[target];
+        for(int i = 1; i <= nums.length; i++) {
+            for(int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if(j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[nums.length][target];
     }
 }
+
+/*
+ * The original problem statement is equivalent to:
+ * Find a subset of nums that need to be positive, and the rest of them negative, such that the sum is equal to target
+ *
+ * Let P be the positive subset and N be the negative subset
+ * For example:
+ * Given nums = [1, 2, 3, 4, 5] and target = 3 then one possible solution is +1-2+3-4+5 = 3
+ * Here positive subset is P = [1, 3, 5] and negative subset is N = [2, 4]
+ *
+ * Then let's see how this can be converted to a subset sum problem:
+ *
+ *                   sum(P) - sum(N) = target
+ * sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+ *                        2 * sum(P) = target + sum(nums)
+ * So the original problem has been converted to a subset sum problem as follows:
+ * Find a subset P of nums such that sum(P) = (target + sum(nums)) / 2
+ *
+ * Note that the above formula has proved that target + sum(nums) must be even
+ */
