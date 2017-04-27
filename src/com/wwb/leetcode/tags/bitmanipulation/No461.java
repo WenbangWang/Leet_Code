7eecfcfae3b930a4f756a1746b64718d68107585
WenbangWang <wenbang.wang@gmail.com>
2017-04-27 16:37:30 -0700

    410,461,477

---
+++ src/com/wwb/leetcode/tags/bitmanipulation/No461.java
@@ -0,0 +1,35 @@
+package com.wwb.leetcode.tags.bitmanipulation;
+
+/**
+ * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ *
+ * Given two integers x and y, calculate the Hamming distance.
+ *
+ * Note:
+ * 0 ≤ x, y < 231.
+ *
+ * Example:
+ *
+ * Input: x = 1, y = 4
+ *
+ * Output: 2
+ *
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *        ↑   ↑
+ *
+ * The above arrows point to positions where the corresponding bits are different.
+ */
+public class No461 {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int result = 0;
+
+        for(int i = 0; i < 32; i++) {
+            result += (xor >>> i) & 1;
+        }
+
+        return result;
+    }
+}
