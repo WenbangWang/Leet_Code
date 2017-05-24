c815827056e3060d7d5e7480c7c13dc6e8fc221e
WenbangWang <wenbang.wang@gmail.com>
2017-05-23 22:17:51 -0700

    261,378,386,TaskCoolDown

---
+++ src/com/wwb/leetcode/medium/No386.java
@@ -0,0 +1,36 @@
+package com.wwb.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer n, return 1 - n in lexicographical order.
+ *
+ * For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+ *
+ * Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+ */
+public class No386 {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        int current = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result.add(current);
+
+            if (current * 10 <= n) {
+                current *= 10;
+            } else if (current % 10 != 9 && current + 1 <= n) {
+                current++;
+            } else {
+                while ((current / 10) % 10 == 9) {
+                    current /= 10;
+                }
+
+                current /= 10;
+                current++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/wwb/leetcode/tags/graph/No261.java
@@ -0,0 +1,48 @@
+package com.wwb.leetcode.tags.graph;
+
+import java.util.Arrays;
+
+/**
+ * Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
+ * write a function to check whether these edges make up a valid tree.
+ *
+ * For example:
+ *
+ * Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
+ *
+ * Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+ *
+ * Note: you can assume that no duplicate edges will appear in edges. Since all edges are undirected,
+ * [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+ */
+public class No261 {
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
+
+        for(int i = 0; i < edges.length; i++) {
+            int x = this.unionFind(parent, edges[i][0]);
+            int y = this.unionFind(parent, edges[i][1]);
+
+            if (x == y) {
+                return false;
+            }
+
+            parent[x] = y;
+        }
+
+        return true;
+    }
+
+    private int unionFind (int[] nums, int i) {
+        if (nums[i] == -1) {
+            return i;
+        }
+
+        return unionFind(nums, nums[i]);
+    }
+}
